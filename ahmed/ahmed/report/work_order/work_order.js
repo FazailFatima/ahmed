0774c28c9024016a34c97d0935564f3aba48a6ba
rajaabdullaharshad <rajaabdullaharshad@gmail.com>
2024-05-07 19:58:15 +0500

    report

---
+++ ahmed/ahmed/report/work_order/work_order.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2024, ahmed and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Work Order"] = {
+	"filters": [
+
+	]
+};
